f6bee72ca27a0130562b9369d21923587cd8d8dd
IreneB26 <bargues.irene@gmail.com>
2022-05-25 20:48:05 +0200

    styles formacion

--- src/pages/index/components/formacion/formacion.jsx
+++ src/pages/index/components/formacion/formacion.jsx
@@ -8,7 +8,7 @@ export default function Formacion(){
     <h3 className="right_title">Title</h3>
     <div className="zigzag-timeline__milestone"></div>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo neque omnis, corrupti cum animi iure sequi amet! Vero sequi dolore mollitia architecto exercitationem magni nemo, officia quia ad, porro id.
+        <p className="info_formacion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo neque omnis, corrupti cum animi iure sequi amet! Vero sequi dolore mollitia architecto exercitationem magni nemo, officia quia ad, porro id.
         At magnam nam dolorum possimus exercitationem officiis sint animi quo dicta enim, numquam, quam delectus architecto? Beatae non ut distinctio repudiandae vitae nihil explicabo eligendi adipisci, qui facere officiis optio?
         Impedit iusto iure modi illum enim laudantium eum hic, aliquam ipsam! Optio dignissimos incidunt provident rem iusto laudantium sequi molestias blanditiis magni sed dolorum architecto laboriosam perspiciatis qui, id atque!
         Reiciendis sint eos nulla
@@ -18,10 +18,10 @@ export default function Formacion(){
 
 
 <div className="zigzag-timeline__item">
-    <h3 className="left_tittle">Title</h3>
+    <h3 className="left_title">Title</h3>
     <div className="zigzag-timeline__milestone"></div>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo neque omnis, corrupti cum animi iure sequi amet! Vero sequi dolore mollitia architecto exercitationem magni nemo, officia quia ad, porro id.
+        <p className="info_formacion left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo neque omnis, corrupti cum animi iure sequi amet! Vero sequi dolore mollitia architecto exercitationem magni nemo, officia quia ad, porro id.
                 At magnam nam dolorum possimus exercitationem officiis sint animi quo dicta enim, numquam, quam delectus architecto? Beatae non ut distinctio repudiandae vitae nihil explicabo eligendi adipisci, qui facere officiis optio?
                 Impedit iusto iure modi illum enim laudantium eum hic, aliquam ipsam! Optio dignissimos incidunt provident rem iusto laudantium sequi molestias blanditiis magni sed dolorum architecto laboriosam perspiciatis qui, id atque!
                 Reiciendis sint eos nulla
@@ -36,7 +36,7 @@ export default function Formacion(){
     <h3 className="right_title">Title</h3>
  
     <div className="zigzag-timeline__milestone"></div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo neque omnis, corrupti cum animi iure sequi amet! Vero sequi dolore mollitia architecto exercitationem magni nemo, officia quia ad, porro id.
+        <p className="info_formacion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo neque omnis, corrupti cum animi iure sequi amet! Vero sequi dolore mollitia architecto exercitationem magni nemo, officia quia ad, porro id.
             At magnam nam dolorum possimus exercitationem officiis sint animi quo dicta enim, numquam, quam delectus architecto? Beatae non ut distinctio repudiandae vitae nihil explicabo eligendi adipisci, qui facere officiis optio?
             Impedit iusto iure modi illum enim laudantium eum hic, aliquam ipsam! Optio dignissimos incidunt provident rem iusto laudantium sequi molestias blanditiis magni sed dolorum architecto laboriosam perspiciatis qui, id atque!
             Reiciendis sint eos nulla
