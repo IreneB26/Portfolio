a943860fd743ab3d14a674d849909622d8f371ea
IreneB26 <bargues.irene@gmail.com>
2022-05-26 21:12:23 +0200

    styles

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,9 +1,10 @@
 import Button from "../../components/button/button";
-import ContactAside from "../../components/contact-sidebar/contact-aside";
 import Titulo from "../../components/titulos/titulo";
 import CardSkill from "./components/card_skill/card_skill";
 import Formacion from "./components/formacion/formacion";
 import Imagen from "./components/imagen_presentacion/imagen";
+
+
 
 import SkillInfo from "../../data/info.json"
 
@@ -20,10 +21,10 @@ export default function Index(){
     return(
         
   
-          <>
+        <>
             
         
-             <section className="section_info">
+            <section className="section_info">
 
                 <article className="image_principal">
                     <Imagen> 
@@ -32,26 +33,27 @@ export default function Index(){
                     </Imagen>
                 </article>
 
-               <article className="info_principal">
+                <article className="info_principal">
                     <Titulo contenido="hi, i`m Irene Bargues"/>
                     <Button contenido="Descargar CV">    
                         <p className="descripcion">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum distinctio quos voluptas vel! Labore vero nam hic minus perspiciatis eum nesciunt consectetur sapiente rerum dicta. Corporis vitae aperiam impedit pariatur!
-                        Aliunt! Similique, praesentium labore! Quasi quisquam corrupti quia facere earum sequi, nesciunt iste temporibus voluptatem eos molestias laboriosam maxime accusamus aperiam, architecto nisi mollitia.</p>
+                        Aliunt! Similique, praesentium labore! Quasi quisquam corrupti quia facere earum sequi, nesciunt iste temporibus voluptatem eos molestias laboriosam maxime accusamus aperiam, architecto nisi mollitia.
+                        </p>
                     </Button>
                 
                 
                    
                </article>
 
-               </section>
+            </section>
 
 
 
         <section className="skills">
 
-        <article className="skill_tittle">
-            <Titulo contenido="Skills"/>
-        </article>
+            <article className="skill_tittle">
+                <Titulo contenido="Skills"/>
+            </article>
 
         <article className="contain_skills">
 
@@ -59,81 +61,87 @@ export default function Index(){
         
             <CardSkill> 
           
-                    <section>
-                        <p className="title_card">Front-end</p>
-                    </section>
+                <section>
+                    <p className="title_card">Front-end</p>
+                </section>
          
-                  <article>
-                        { front.map((img) => 
+                <article>
+                    { front.map((img) => 
                        
-                       <div class="tooltip">
-                           <img className="img_skill" src={`${img.image}`}></img>
-                        <span class="tooltiptext">{img.name}</span>
+                        <div class="tooltip">
+
+                           <img className="img_skill" src={`${img.image}`} alt={`${img.alt}`}></img>
+
+                            <span class="tooltiptext">{img.name}</span>
+
                         </div>
                         
-                      
- 
-                        )}  
+                     )}  
                     </article>
             </CardSkill>
          
             
-        <CardSkill> 
-        <section>
-                        <p className="title_card">Backend</p>
-                    </section>
+            <CardSkill> 
+                <section>
+                    <p className="title_card">Backend</p>
+                </section>
          
-                  <article>
-                        { back.map((img) => 
+                <article>
+                    { back.map((img) => 
                        
-                       <div class="tooltip">
-                       <img className="img_skill" src={`${img.image}`}></img>
-                    <span class="tooltiptext">{img.name}</span>
-                    </div>
+                        <div class="tooltip">
 
-                        )}  
-                    </article>
-        </CardSkill>
+                           <img className="img_skill" src={`${img.image}`} alt={`${img.alt}`}></img>
 
-        <CardSkill> 
-        <section>
-                        <p className="title_card">Database</p>
-                    </section>
-         
-                  <article>
-                        { database.map((img) => 
-                       
-                           <div class="tooltip">
-                           <img className="img_skill" src={`${img.image}`}></img>
-                        <span class="tooltiptext">{img.name}</span>
-                        </div>
+                            <span class="tooltiptext">{img.name}</span>
 
-                        )}  
-                    </article>
-        </CardSkill>
-
-        <CardSkill> 
-        <section>
-                        <p className="title_card">Tools</p>
-                    </section>
-         
-                  <article>
-                        { tools.map((img) => 
-                       
-                           <div class="tooltip">
-                           <img className="img_skill" src={`${img.image}`}></img>
-                        <span class="tooltiptext">{img.name}</span>
                         </div>
                         
-                        )}  
+                     )}  
                     </article>
-        </CardSkill>
+            </CardSkill>
 
-        </article>
-
+            <CardSkill> 
+                <section>
+                    <p className="title_card">Database</p>
+                </section>
             
+                <article>
+                    { database.map((img) => 
+                       
+                        <div class="tooltip">
 
+                           <img className="img_skill" src={`${img.image}`} alt={`${img.alt}`}></img>
 
+                            <span class="tooltiptext">{img.name}</span>
+
+                        </div>
+                        
+                     )}  
+                    </article>
+            </CardSkill>
+
+            <CardSkill> 
+                <section>
+                    <p className="title_card">Tools</p>
+                </section>
+            
+                <article>
+                    { tools.map((img) => 
+                       
+                        <div class="tooltip">
+
+                           <img className="img_skill" src={`${img.image}`} alt={`${img.alt}`}></img>
+
+                            <span class="tooltiptext">{img.name}</span>
+
+                        </div>
+                        
+                     )}  
+                    </article>
+            </CardSkill>
+
+            </article>
 
         </section>
 
